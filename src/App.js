1e99d4ca63472320e7662a91c2013ecff37e53cd
YZTangent <yuanzheng.tan.tyz@gmail.com>
2022-07-26 07:05:33 +0800

    Added time field

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
 import './App.css';
 import DatePicker from 'sassy-datepicker';
+import {
+  Flex,
+  Box,
+  FormControl,
+  FormLabel,
+  Input,
+  Checkbox,
+  Stack,
+  Link,
+  Button,
+  Heading,
+  Text,
+  useColorModeValue,
+  Tooltip,
+  Center
+} from '@chakra-ui/react';
 
 function App() {
   const onChange = (date) => {
@@ -7,7 +23,29 @@ function App() {
   };
 
   return (
-    <DatePicker onChange={onChange} />
+    <Flex
+        minH={'100vh'}
+        align={'center'}
+        justify={'center'}
+        bg={useColorModeValue('gray.50', 'gray.800')}>
+        <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
+          <Box
+            rounded={'lg'}
+            bg={useColorModeValue('white', 'gray.700')}
+            boxShadow={'lg'}
+            p={8}>
+            <Stack spacing={4}>
+              <FormControl id="email" >
+                <FormLabel>Time</FormLabel>
+                <Input 
+                  type="time" 
+                  placeholder="TTTT" />
+              </FormControl>
+              <DatePicker />
+            </Stack>
+          </Box>
+        </Stack>
+      </Flex>
   );
 }
 export default App;

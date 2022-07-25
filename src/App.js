1fbf41c7be49c56a27139b6b93de65034daba4fc
YZTangent <yuanzheng.tan.tyz@gmail.com>
2022-07-26 06:51:29 +0800

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import DatePicker from 'sassy-datepicker';
 
 function App() {
+  const onChange = (date) => {
+    console.log(date.toString());
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <DatePicker onChange={onChange} />
   );
 }
-
 export default App;
